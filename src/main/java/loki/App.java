e8d72181d26d4d0d5ffcf22195da62b94d6136db
Jordan Glassman <jglassman@cloudbees.com>
2018-06-26 18:45:55 -0400

    update

---
+++ src/main/java/loki/App.java
@@ -0,0 +1,14 @@
+package loki;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        // insert bad code here
+    }
+}
