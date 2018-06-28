44acded07ce9936ada293d81a96c5444740e47ca
Michael Gagliardo <mgagliardo91@gmail.com>
2018-06-28 10:25:58 -0400

    ANALYTICS-3001 Bad echos

--- src/main/java/loki/App.java
+++ src/main/java/loki/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         // insert bad code here
         System.out.println( "Hello World!" );
+        System.out.println( "Hello World!"
     }
 }
